caaf9177e7347ccb04192af753c420bc3a24ab0e
Scott Pierce <scott.pierce@centro.net>
2015-11-30 10:50:52 -0600

    added typemap xml file generator

---
+++ typemap_gen.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+#
+# Example Usage: 
+#   ./typemap_gen.rb /Library/Fonts/*.ttf > .magic/type.xml
+
+puts "<typemap>"
+ARGV.each{|e|
+  base=File.basename(e)
+  f,n=base.split('.')
+  puts %Q{<type format="#{f}" name="#{n}" glyphs="#{e}" />}
+}
+puts "</typemap>"
