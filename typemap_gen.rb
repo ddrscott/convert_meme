80190ee53da75bc1fc6a8793f1b51a36be005757
Scott Pierce <scott.pierce@centro.net>
2015-12-01 05:36:29 -0600

    simple typemap generator

--- typemap_gen.rb
+++ typemap_gen.rb
@@ -3,10 +3,12 @@
 # Example Usage: 
 #   ./typemap_gen.rb /Library/Fonts/*.ttf > .magic/type.xml
 
-puts "<typemap>"
+puts '<typemap>'
 ARGV.each{|e|
   base=File.basename(e)
-  f,n=base.split('.')
-  puts %Q{<type format="#{f}" name="#{n}" glyphs="#{e}" />}
+  if base =~ /^[[:ascii:]]+$/
+    f,n=base.split('.')
+    puts %Q{<type format="#{f}" name="#{n}" glyphs="#{e}" />}
+  end
 }
-puts "</typemap>"
+puts '</typemap>'
